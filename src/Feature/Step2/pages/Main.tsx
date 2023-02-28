7d7d97a778a15a63e9eaa202e590902ad99eabd7
Thien Bao <truongbao901@gmail.com>
2023-02-28 21:47:17 +0700

    cm to pull

--- src/Feature/Step2/pages/Main.tsx
+++ src/Feature/Step2/pages/Main.tsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+const Div = styled('div')(({ theme }) => ({
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+}));
+
+function Step2() {
+    return (
+        <Container maxWidth="lg">
+            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+                <Div>{"This div's text looks like that of a button."}</Div>;
+            </Box>
+        </Container>
+    )
+}
+
+export default React.memo(Step2);
