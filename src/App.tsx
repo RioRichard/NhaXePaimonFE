d428cfdf3a6d5d8755fce22910d4c7daec4ff9e0
InoriZ <quocthai.jy9a13@gmail.com>
2023-03-04 20:34:11 +0700

    feat: create Book Ticket Step 1 Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css';
 import { Header } from './layout/Header';
 import { Step1 } from './feature/BookTicket/page/Step1';
 
+
+
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
-    <Step1></Step1>
+      <Step1></Step1>
     </div>
   );
 }
