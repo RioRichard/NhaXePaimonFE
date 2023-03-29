f1b855e2d0289a06aec6204c0e6ce62646d74898
InoriZ <quocthai.jy9a13@gmail.com>
2023-03-29 09:47:13 +0700

    change branch

--- src/Feature/BookTicket/page/Step1Page.tsx
+++ src/Feature/BookTicket/page/Step1Page.tsx
@@ -1,21 +1,27 @@
 import Step1 from "../components/Step1";
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { Order } from "../../Order/types";
+import { updateFormData } from "../BookTicketSlice";
+import { history } from "../../../utils";
 
 export default function Step1Page() {
     const dispatch = useAppDispatch();
     const handleSubmit = (values: Order) => {
-
         return new Promise((resolve) => {
             setTimeout(() => {
                 const newData = {
-                    ...values,
-                    from_id: values.from_id,
-                    to_id: values.to_id,
+                    from_id: values?.from_id,
+                    to_id: values?.to_id,
                 };
-            
+                const formData: any = { ...newData };
+                const data: any = {
+                    step: 'step1',
+                    formData,
+                };
+                dispatch(updateFormData(data))
+                history.push('/chon-ghe')
                 resolve(true);
-            }, 1000);
+            }, 10);
         });
     }
     return (
